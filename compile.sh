934baa7273f9718a5a8edac22c9da0f192c3bd17
takahiroharada <takahiroharada@gmail.com>
2016-11-26 18:10:35 -0800

    File name fix.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 swig -c++ -python func.i
-g++ -shared -fPIC -o func.so *.cpp *.cxx -I/usr/include/python2.7/ -lpython2.7
+g++ -shared -fPIC -o _func.so *.cpp *.cxx -I/usr/include/python2.7/ -lpython2.7
 
 
